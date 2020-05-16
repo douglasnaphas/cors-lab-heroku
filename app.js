637bdafacba12297eb9faf793adfdbe6d4f67326
Douglas Naphas <douglasnaphas@gmail.com>
2020-05-16 15:10:41 -0400

    Try to recreate a CORS POST issue
    https://github.com/expressjs/cors/issues/197

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 var express = require("express");
 var app = express();
 const cors = require("cors");
+const router = express.Router();
 const PORT = process.env.PORT || 5000;
+
+const CREATE_CHECKOUT_SESSION_ROUTE = "/api/payment/create-checkout-session";
+const checkJwt = (req, res, next) => next();
 
 app.options("*", cors());
 app.use(cors());
+
+router.post(CREATE_CHECKOUT_SESSION_ROUTE, checkJwt, async (req, res) => {
+  return res.send({ data: "some data" });
+});
+
+app.use(router);
 
 app.get("/", function (req, res) {
   res.send({
